f49d10a129a61ff1f193c4c8429bd5ee779e50a1
jaycePark <jaycepark@lgcns.com>
2024-02-19 14:50:28 +0900

    test

--- apps/shell/src/App.tsx
+++ apps/shell/src/App.tsx
@@ -1,7 +1,8 @@
-import React, { Suspense } from 'react';
+import React, { Children, Suspense } from 'react';
 
 
 const Button2 = React.lazy(() => import('test2/testButton'));
+const Button3 = React.lazy(() => import('test2/testButton2'));
 
 const App = () => {
     return (
@@ -13,6 +14,7 @@ const App = () => {
             Federated React App
             <div>
               <Button2/>
+              <Button3 onclick={() => {alert("123123")} }>fdsa </Button3>
             </div>
           </main>
 
